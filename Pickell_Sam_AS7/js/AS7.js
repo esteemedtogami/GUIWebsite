5fc8abf413da468468c6dcee07c2eb2a2e1377da
esteemedtogami <sam.pickell01@gmail.com>
2018-11-21 15:38:38 -0500

    Finished hw7!

--- Pickell_Sam_AS7/js/AS7.js
+++ Pickell_Sam_AS7/js/AS7.js
@@ -4,31 +4,109 @@ UMass Lowell CS Student, COMP 4610 GUI I
 November 18, 2018
 Assignment 7: Update AS6 to have jQuery validation
 */
-var h_start, h_end, v_start, v_end;
+var h_start, h_end, v_start, v_end, my_table;
+
+//check form validity even when button is "button" and not "submit"
+//Source: https://stackoverflow.com/questions/13671710/jquery-form-validation-on-button-click
+$('#my_button').on('click', function() {
+    $("#input_form").valid();
+});
+
+//Learned how to set up rules and messages from Prof. Jessie Heines' website:
+//https://jesseheines.com/~heines/91.461/91.461-2012-13f/461-lecs/code/jmh-table-v5.html?xBegin=2&xEnd=5&yBegin=5&yEnd=7
+$().ready(function(){
+    $('#input_form').validate({
+        //The rules tell the validator what to look out for
+        rules: {
+            horizontal_start:{
+                //e.g. the field is required, only accepts digits, and only
+                // accepts numbers within a certain range
+                    required: true,
+                    digits: true,
+                    range:[1,14]
+            },
+            horizontal_end:{
+                    required: true,
+                    digits: true,
+                    range:[2,15]
+            },
+            vertical_start:{
+                    required: true,
+                    digits: true,
+                    range:[1,14]
+            },
+            vertical_end:{
+                    required: true,
+                    digits: true,
+                    range:[2,15]
+            }
+        },
+
+        //customized messages depending on the type of error
+        messages: {
+            horizontal_start:{
+                    required: function(){
+                        return "<em>You MUST indicate a horizontal start!</em><br></br>";
+                    },
+                    digits: function(){
+                        return "<em>Digits are the only valid input.</em><br></br>";
+                    },
+                    range: function(){
+                        return "<em>Must be a number in range 1-14.</em><br></br>";
+                    }
+            },
+            horizontal_end:{
+                    required: function(){
+                        return "<em>You MUST indicate a horizontal end!</em><br></br>";
+                    },
+                    digits: function(){
+                        return "<em>Digits are the only valid input.</em><br></br>";
+                    },
+                    range: function(){
+                        return "<em>Must be a number in range 2-15.</em><br></br>";
+                    }
+            },
+            vertical_start:{
+                    required: function(){
+                        return "<em>You MUST indicate a vertical start!</em><br></br>";
+                    },
+                    digits: function(){
+                        return "<em>Digits are the only valid input.</em><br></br>";
+                    },
+                    range: function(){
+                        return "<em>Must be a number in range 1-14.</em><br></br>";
+                    }
+            },
+            vertical_end:{
+                    required: function(){
+                        return "<em>You MUST indicate a vertical end!</em><br></br>";
+                    },
+                    digits: function(){
+                        return "<em>Digits are the only valid input.</em><br></br>";
+                    },
+                    range: function(){
+                        return "<em>Must be a number in range 2-15.</em>";
+                    }
+            }
+        }
+    });
+});
 
 function get_numbers(){
 
     //Get the values from the forms, and check their validity
     h_start = parseInt(document.getElementById('horizontal_start').value, 10);
-    if(err_handle(h_start, "Horizontal Start", 1, 14) == 0) return;
     h_end = parseInt(document.getElementById('horizontal_end').value, 10);
-    if(err_handle(h_end, "Horizontal End", 2, 15) == 0) return;
     v_start = parseInt(document.getElementById('vertical_start').value, 10);
-    if (err_handle(v_start, "Vertical Start", 1, 14) == 0) return;
     v_end = parseInt(document.getElementById('vertical_end').value, 10);
-    if (err_handle(v_end, "Veritcal End", 2, 15) == 0) return;
 
-    //make sure the starting value is smaller than the ending value
-    if(size_comp(h_start, h_end, "horizontal") == 0) return;
-    if(size_comp(v_start, v_end, "vertical") == 0) return;
-
-    //Call the function that will build the table
     generate_table();
+
 }
 
 function generate_table(){
     //create a variable that will hold the table information
-    var my_table = "<table>";
+    my_table = "<table>";
 
     //double for loop to track the vertical and horizontal coordinates
     //   of the table
@@ -64,25 +142,4 @@ function generate_table(){
     my_table+="</table>";
     //send the table data to the div in the HTML doc
     document.getElementById('mult_tab').innerHTML = my_table;
-}
-
-//Checks validity of parameters, takes in the value, the variable name,
-// and the upper and lower bounds of the range
-function err_handle(val, name, little, big){
-    if((val < little) || (val > big) || !(val === parseInt(val, 10)))
-    {
-        alert("Error. " + name + " must be a number in the range from " + little + " to " + big + ".");
-        return 0;
-    }
-    return 1;
-}
-
-//Checks that the value entered in the "smaller" section is in fact smaller
-function size_comp(the_start, the_end, name){
-    if(the_start > the_end)
-    {
-        alert("Error. The " + name + " starting value must be smaller than the ending value.");
-        return 0;
-    }
-    return 1;
 }
